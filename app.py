94d2c72dbd870bb20a0121947026922a8e0e39d2
majdimbarek <you@example.com>
2023-05-21 12:52:51 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,53 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///birthdays.db")
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    name = request.form.get("name")
+    month = request.form.get("month")
+    day = request.form.get("day")
+    if request.method == "POST":
+
+        # TODO: Add the user's entry into the database
+        db.execute("INSERT INTO birthdays (name,month,day) VALUES(?,?,?)", name, month, day)
+        # DELETE an empty input was inserted into a birthdays table
+        db.execute("DELETE FROM birthdays WHERE name IS NULL OR trim(name) = ''")
+        db.execute("DELETE FROM birthdays WHERE month IS NULL OR month < 1 OR month > 12 OR trim(month) = ''")
+        db.execute("DELETE FROM birthdays WHERE day IS NULL OR day < 1 OR day > 31 OR trim(day) = ''")
+
+
+        return redirect("/")
+
+    else:
+
+        # TODO: Display the entries in the database on index.html
+        display = db.execute("SELECT DISTINCT * FROM birthdays ORDER BY month")
+        return render_template("index.html", displays = display)
+
+# make a delete button
+@app.route("/delete", methods = ["POST", "GET"])
+def delete():
+    id  = request.form.get("id")
+    if request.method == "POST":
+        db.execute("DELETE FROM birthdays WHERE id = ?", id)
+        return redirect("/")
